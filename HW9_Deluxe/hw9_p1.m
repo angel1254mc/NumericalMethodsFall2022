4bb454caf328dabaaf0f3529bd9c3a54da46b283
Angel Lopez Pol <angellopezpol1254@gmail.com>
2023-04-28 17:59:40 -0400

    New changes lets goooo 😎😎😎

--- HW9_Deluxe/hw9_p1.m
+++ HW9_Deluxe/hw9_p1.m
@@ -5,17 +5,18 @@ some_data = load("hw9_p1.mat");
     that passes thru all of the (x,y) value pairs provided 🤓. The formula is relatively shrimple
     but it can be confusing to implement in code
 %}
-    
+
+lagrange_plot(some_data.x, some_data.y, some_data.x(1), some_data.x(length(some_data.x)))
 
 % Let's make a function that actually "draws out" or "plots" this lagrange polynomial
 % We can do this by determininng an interval that we want to plot over, which in this case
 % would just be the interval from [lowest x value, highest x value]
-function [x, y] = lagrange_plot(x, y, plot_start, plot_end)
+function [x_lagrange, y_lagrange] = lagrange_plot(x, y, plot_start, plot_end)
 
     data_length = length(x);
     % What is the interval we are plotting over? plot_start to plot_end, with a bunch of small steps in between
-    interval_of_plot = plot_start:0.01:plot_end;
-    P = zeros(length(interval_of_plot), 2) % P is going to be a matrix with 2 columns, and n rows, where the columns  correspond to x and y respectively
+    interval_of_plot = plot_start:1:plot_end;
+    P = zeros(length(interval_of_plot), 2); % P is going to be a matrix with 2 columns, and n rows, where the columns  correspond to x and y respectively
 
     % For each x value that we are plotting, use the lagrange polynomial to get the y_value
     for n = 1:length(interval_of_plot)
@@ -38,7 +39,7 @@ function [x, y] = lagrange_plot(x, y, plot_start, plot_end)
         % Note how for x1, (x-x1) is not present in the top. This goes for all xi, (x - xi) won't be present in the fraction!
 
         % Setting up our x value
-        P(n, 1) = interval_of_plot(n)
+        P(n, 1) = interval_of_plot(n);
         
         for i = 1:length(x)
 
@@ -50,9 +51,9 @@ function [x, y] = lagrange_plot(x, y, plot_start, plot_end)
                 % Only calculate for everything that is not xi
                 % numerator = numerator*(stuff) This is like (x - xi) * (x - x(i+1)) * (x - x(i+2))...
                 % denominator = denominator*(stuff) This is like (x1 - x2) * (x1 - x3) ...
-                if (j != i)
-                    numerator = numerator*(P(n, i) - x(j))
-                    denominator = denominator(x(i) - x(j))
+                if (j ~= i)
+                    numerator = numerator*(P(n, 1) - x(j));
+                    denominator = denominator*(x(i) - x(j));
                 end
                 
             end
@@ -61,8 +62,11 @@ function [x, y] = lagrange_plot(x, y, plot_start, plot_end)
         end
     end
 
-    plot(P(:, 1), P((:, 2)))
+    plot(P(:, 1), P(:, 2))
     hold on;
-    plot(x, y)
+    plot(x, y);
+
+    x_lagrange = P(:, 1);
+    y_lagrange = P(:, 2);
 
 end
